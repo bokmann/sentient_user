260ffa321876049e5cf4b9f2acde8c6abe826d4b
bokmann <dbock@codesherpas.com>
2012-10-04 00:27:26 -0400

    adding simplecov to the project

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require 'simplecov'
+SimpleCov.start
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
